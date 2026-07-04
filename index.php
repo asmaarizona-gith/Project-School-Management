07fda92d31f08b9fae34768c0a4c185d5906c3e7
asmaarizona-gith <asmaarizona@gmail.com>
2026-07-04 22:44:50 +0300

    chore: add gitignore to protect config files

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 session_start();
 if (isset($_SESSION['user_id'])) {
     header('Location: dashboard/index.php');
+    // Kani waa nidaamka maamulka dugsiga (School Management System)
 } else {
     header('Location: auth/login.php');
 }
